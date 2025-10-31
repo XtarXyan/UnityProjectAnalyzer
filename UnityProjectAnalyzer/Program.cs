7d7d198eea518e9bad5727e014913b39401f1ba7
Eusebiu Pusca <eusebiup@proton.me>
2025-10-31 22:38:08 +0100

    setup project directory

---
+++ UnityProjectAnalyzer/Program.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UnityProjectAnalyzer;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Unity project analyzer");
+    }
+}
